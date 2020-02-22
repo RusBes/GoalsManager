4992f68c1b5dd77cea6096df2580f5d39669e094
BigBlackCatS <charp1237@gmail.com>
2020-02-22 17:50:21 +0300

    TaskTablesAdded

---
+++ Scheduler/Notification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Scheduler
+{
+	/// <summary>
+	/// Notification Table Info
+	/// </summary>
+	public class Notification
+	{
+		public long NotificationId { get; set; }
+
+		[ForeignKey("Task")]
+		public long? TaskId { get; set; }
+
+		public DateTime DateTimeStart { get; set; }
+
+		public virtual Task Task { get; set; }
+	}
+}
---
+++ Scheduler/TaskContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Scheduler
+{
+	/// <summary>
+	/// Task Table Info
+	/// </summary>
+	public class TaskContext : DbContext
+	{
+		public DbSet<Task> Tasks { get; set; }
+
+		public DbSet<Notification> Notifications { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder options)
+			=> options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=schedulerdb;Trusted_Connection=True;");
+	}
+}

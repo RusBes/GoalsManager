a7b1da332fb24f0ad968260f354e2900915f6213
BigBlackCatS <charp1237@gmail.com>
2020-02-22 17:50:21 +0300

    TaskTablesAdded

---
+++ Scheduler/Migrations/20200222142020_InitialCreate.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scheduler.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    TaskId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskParentId = table.Column<long>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Status = table.Column<int>(nullable: false),
+                    Group = table.Column<string>(nullable: true),
+                    DateTimeStart = table.Column<DateTime>(nullable: false),
+                    DateTimeEnd = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.TaskId);
+                    table.ForeignKey(
+                        name: "FK_Tasks_Tasks_TaskParentId",
+                        column: x => x.TaskParentId,
+                        principalTable: "Tasks",
+                        principalColumn: "TaskId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    NotificationId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskId = table.Column<long>(nullable: true),
+                    DateTimeStart = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.NotificationId);
+                    table.ForeignKey(
+                        name: "FK_Notifications_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "TaskId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notifications_TaskId",
+                table: "Notifications",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_TaskParentId",
+                table: "Tasks",
+                column: "TaskParentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notifications");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ Scheduler/Task.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Scheduler
+{
+	/// <summary>
+	/// Task Table Info
+	/// </summary>
+	public class Task
+	{
+		public long TaskId { get; set; }
+
+		[ForeignKey("Goal")]
+		
+		public long? TaskParentId { get; set; }
+
+		public string Name { get; set; }
+
+		public int Status { get; set; }
+
+		public string Group { get; set; }
+
+		public DateTime DateTimeStart { get; set; }
+
+		public DateTime DateTimeEnd { get; set; }
+
+		public virtual ICollection<Notification> Notifications { get; set; } = new List<Notification>();
+
+		public virtual Task TaskParent { get; set; }
+
+		public virtual ICollection<Task> Goals { get; set; } = new List<Task>();
+	}
+}
+

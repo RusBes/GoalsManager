51b93b1ebe911d41326ac053e23b5d98a0f6ea60
BigBlackCatS <charp1237@gmail.com>
2020-03-12 21:18:51 +0300

    Goal Date Fields changed to nullable

--- Scheduler/Migrations/20200312181525_ChangeGoalDateFieldsToNulable.Designer.cs
+++ Scheduler/Migrations/20200312181525_ChangeGoalDateFieldsToNulable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Scheduler;
 
 namespace Scheduler.Migrations
 {
     [DbContext(typeof(GoalContext))]
-    partial class GoalContextModelSnapshot : ModelSnapshot
+    [Migration("20200312181525_ChangeGoalDateFieldsToNulable")]
+    partial class ChangeGoalDateFieldsToNulable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

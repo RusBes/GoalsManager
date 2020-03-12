1f9de9c2107b293c4ebef634c8c3c0e4119abf8a
BigBlackCatS <charp1237@gmail.com>
2020-03-12 21:18:51 +0300

    Goal Date Fields changed to nullable

---
+++ Scheduler/Migrations/20200312181525_ChangeGoalDateFieldsToNulable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scheduler.Migrations
+{
+    public partial class ChangeGoalDateFieldsToNulable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTimeStart",
+                table: "Goals",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTimeEnd",
+                table: "Goals",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTimeStart",
+                table: "Goals",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTimeEnd",
+                table: "Goals",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldNullable: true);
+        }
+    }
+}
